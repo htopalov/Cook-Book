317f29f3473317d006d801434eb9435b08542265
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-07-30 18:50:31 +0300

    changed titles for recipe list depending on path

--- cook-book-ui/src/components/Recipes/RecipesList.js
+++ cook-book-ui/src/components/Recipes/RecipesList.js
@@ -59,7 +59,7 @@ const RecipesList = () => {
         <div className="container-xxl py-5">
         <div className="container">
             <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h1 className="section-title ff-secondary text-center text-primary fw-normal mt-5 mb-5">Recipes</h1>
+                <h1 className="section-title ff-secondary text-center text-primary fw-normal mt-5 mb-5">{path === '/all' ? 'Recipes' : 'My recipes'}</h1>
             </div>
             <div className="tab-className text-center wow fadeInUp" data-wow-delay="0.1s">
                 <div className="tab-content">
